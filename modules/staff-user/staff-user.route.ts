5097dc06e1485dcb11975ea0b581250f490ec5c9
Ganesh Nayanajith <32520964+ganeshnayanajith@users.noreply.github.com>
2023-07-31 08:33:52 +0530

    api docs for user login and staff user login

--- modules/staff-user/staff-user.route.ts
+++ modules/staff-user/staff-user.route.ts
@@ -3,6 +3,54 @@ import StaffUserController from './staff-user.controller';
 
 const router = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Staff users
+ *   description: API endpoints for staff users management
+ * definitions:
+ *   LoginStaffUserDTO:
+ *     type: object
+ *     properties:
+ *       username:
+ *         type: string
+ *       password:
+ *         type: string
+ */
+
+/**
+ * @swagger
+ * /staff-user/login:
+ *   post:
+ *     summary: Login staff user
+ *     tags: [Staff users]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/definitions/LoginStaffUserDTO'
+ *     responses:
+ *       200:
+ *         description: Login Successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: number
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     userId:
+ *                       type: string
+ *                     accessToken:
+ *                       type: string
+ *                 error:
+ *                   type: null
+ */
 router.post('/login', StaffUserController.login);
 
 export default router;
--- modules/user/user.route.ts
+++ modules/user/user.route.ts
@@ -20,6 +20,13 @@ const router = Router();
  *         type: string
  *       password:
  *         type: string
+ *   LoginUserDTO:
+ *     type: object
+ *     properties:
+ *       email:
+ *         type: string
+ *       password:
+ *         type: string
  */
 
 /**
@@ -54,6 +61,40 @@ const router = Router();
  *                   type: null
  */
 router.post('/register', UserController.register);
+
+/**
+ * @swagger
+ * /user/login:
+ *   post:
+ *     summary: Login user
+ *     tags: [Users]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/definitions/LoginUserDTO'
+ *     responses:
+ *       200:
+ *         description: Login Successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: number
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     userId:
+ *                       type: string
+ *                     accessToken:
+ *                       type: string
+ *                 error:
+ *                   type: null
+ */
 router.post('/login', UserController.login);
 
 export default router;
