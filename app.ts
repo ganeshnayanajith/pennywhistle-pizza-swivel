8b0e68205db85c82b198094225858b598edd7ab7
Ganesh Nayanajith <32520964+ganeshnayanajith@users.noreply.github.com>
2023-08-01 20:02:26 +0530

    added comments for app.ts

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+// importing required modules and libraries
 import createError from 'http-errors';
 import express, { Request, Response, NextFunction } from 'express';
 import path from 'path';
@@ -8,6 +9,7 @@ import { API } from './lib/constant';
 import { connectDB } from './lib/database';
 import { swaggerUi, specs } from './lib/swagger-config';
 
+// importing route modules
 import indexRouter from './modules';
 import userRouter from './modules/user/user.route';
 import staffUserRouter from './modules/staff-user/staff-user.route';
@@ -15,26 +17,39 @@ import productRouter from './modules/product/product.route';
 import orderRouter from './modules/order/order.route';
 import reportRouter from './modules/report/report.route';
 
+// creating the express application instance
 const app = express();
 
 (async () => {
+  // establishing the database connection
   await connectDB();
 })();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.use(helmet());
-app.use(loggerMorgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// middleware setup
+
+// adding security headers to HTTP response
+app.use(helmet());
+// logging HTTP requests in development mode
+app.use(loggerMorgan('dev'));
+// parsing incoming JSON data
+app.use(express.json());
+// parsing URL-encoded data
+app.use(express.urlencoded({ extended: false }));
+// parsing cookies from the request
+app.use(cookieParser());
+
+// base API path
 const BASE_PATH = API.BASE_PATH;
 
+// setting up swagger documentation
 app.use(`${BASE_PATH}/docs`, swaggerUi.serve, swaggerUi.setup(specs));
+
+// setting up routes for different modules
 app.use(`${BASE_PATH}/`, indexRouter);
 app.use(`${BASE_PATH}/user`, userRouter);
 app.use(`${BASE_PATH}/staff-user`, staffUserRouter);
@@ -58,4 +73,5 @@ app.use(function (err: createError.HttpError, req: Request, res: Response, next:
   res.render('error');
 });
 
+// exporting the express application instance
 export default app;
