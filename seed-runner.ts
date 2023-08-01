152877b9336bd08b33f36567a97cada919f40b77
Ganesh Nayanajith <32520964+ganeshnayanajith@users.noreply.github.com>
2023-08-01 20:09:12 +0530

    added comments for seed-runner.ts

--- seed-runner.ts
+++ seed-runner.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import dotenv from 'dotenv';
 
+// loading environment variables from .env file
 dotenv.config({ path: path.resolve(__dirname, './.env') });
 
 import { StaffUser } from './modules/staff-user/staff-user.model';
@@ -11,9 +12,10 @@ import { Types } from 'mongoose';
 
 async function seedData() {
   try {
-
+    // establishing the database connection
     await connectDB();
 
+    // creating a new StaffUser with the role of Admin
     const newUser = new StaffUser({
       _id: new Types.ObjectId(),
       username: 'Admin',
@@ -28,10 +30,12 @@ async function seedData() {
   } catch (error) {
     logger.error(`Error seeding data: ${error}`);
   } finally {
+    // closing the database connection after the data seed process (regardless of success or failure)
     await disconnectDB();
   }
 }
 
+// immediately-invoked function expression (IIFE) to call the seedData function
 (async () => {
   await seedData();
 })();
