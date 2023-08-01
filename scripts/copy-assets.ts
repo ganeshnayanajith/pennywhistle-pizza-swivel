5f8d5429e62bb1b95556eb4457b69b67d3a7751f
Ganesh Nayanajith <32520964+ganeshnayanajith@users.noreply.github.com>
2023-08-01 20:15:04 +0530

    added comments for copy-assets.ts

--- scripts/copy-assets.ts
+++ scripts/copy-assets.ts
@@ -1,5 +1,6 @@
 import * as shell from 'shelljs';
 
-// Copy all the view templates
+// copying all the view templates from the 'views' directory to the 'dist/' directory
 shell.cp('-R', 'views', 'dist/');
+// copying all the static assets from the 'public' directory to the 'dist/' directory
 shell.cp('-R', 'public', 'dist/');
