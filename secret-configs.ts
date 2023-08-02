260a1965812a51dfb8eabf808e51b7f32f2d0998
Ganesh Nayanajith <32520964+ganeshnayanajith@users.noreply.github.com>
2023-08-02 12:46:45 +0530

    configs and env modification

--- secret-configs.ts
+++ secret-configs.ts
@@ -1,4 +1,6 @@
 export default {
-  JWT_SECRET: process.env.JWT_SECRET || '',
-  MONGODB_URI: process.env.MONGODB_URI || '',
+  PORT: process.env.PORT || 4000,
+  BASE_URL: process.env.BASE_URL || 'http://localhost:4000',
+  JWT_SECRET: process.env.JWT_SECRET || 'test',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pennywhistle-pizza-swivel',
 };
